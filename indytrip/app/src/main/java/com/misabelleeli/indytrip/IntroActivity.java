c2721fc2673e5e5b500dec7ff5d37ad67a537a17
Inhwan Lee <ihlee01@hotmail.com>
2014-07-26 14:23:17 -0400

    Intro Activity done

--- indytrip/app/src/main/java/com/misabelleeli/indytrip/IntroActivity.java
+++ indytrip/app/src/main/java/com/misabelleeli/indytrip/IntroActivity.java
@@ -1,36 +1,43 @@
 package com.misabelleeli.indytrip;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 
 public class IntroActivity extends Activity {
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Full page logo screen - remove title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_intro);
+
+        // Delay for 1 sec
+        handler = new Handler();
+        handler.postDelayed(irun, 1000);
     }
 
+    Runnable irun = new Runnable() {
+        public void run() {
+            Intent i = new Intent(IntroActivity.this, DateActivity.class);
+            startActivity(i);
+            finish();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.intro, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+            // Screen fade in & out effect
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
         }
-        return super.onOptionsItemSelected(item);
+    };
+
+    public void onBackPressed() {
+        super.onBackPressed();
+        handler.removeCallbacks(irun);
     }
 }
