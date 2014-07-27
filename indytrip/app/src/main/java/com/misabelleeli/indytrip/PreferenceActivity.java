e2ee4e0bb0554036d568622c90c19bf2edd94fa8
Isabel Lee <m.isabellee@hotmail.com>
2014-07-26 21:30:47 -0400

    Added seekbar for Preference

--- indytrip/app/src/main/java/com/misabelleeli/indytrip/PreferenceActivity.java
+++ indytrip/app/src/main/java/com/misabelleeli/indytrip/PreferenceActivity.java
@@ -7,9 +7,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class PreferenceActivity extends Activity {
     Button nextButton;
+    private SeekBar seekBarControl = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +26,26 @@ public class PreferenceActivity extends Activity {
                 startActivity(i);
             }
         });
+
+
+        setContentView(R.layout.activity_preference);
+
+        seekBarControl = (SeekBar) findViewById(R.id.budget_bar);
+        seekBarControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChanged = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
 
@@ -29,6 +53,7 @@ public class PreferenceActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.preference, menu);
+
         return true;
     }
 
