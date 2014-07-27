70d61e876dd23d681b52ded814f6713c8c268c63
m-isabel <isabellee@Isabels-MacBook-Pro.local>
2014-07-27 14:54:19 -0400

    Added GoogleMaps implementation

--- indytrip/app/src/main/java/com/misabelleeli/indytrip/ResultActivity.java
+++ indytrip/app/src/main/java/com/misabelleeli/indytrip/ResultActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -53,13 +54,15 @@ public class ResultActivity extends Activity {
         6th param - int latitude
         */
 
-
         result_headers.add("Day 1");
         result_headers.add("Day 2");
-        Result a = new Result("none","Monument","Dont know","awesome place",0,0);
-        Result b = new Result("none","Monument2","Dont know","awesome place2",0,0);
-        Result c = new Result("none","Monument3","Dont know","awesome place3",0,0);
-        Result d = new Result("none","Monument4","Dont know","awesome place4",0,0);
+        Result a = new Result("lucas","Lucas Oil Stadium","500 S Capitol Ave","awesome place",0,0);
+        Result b = new Result("ncaa","NCAA Hall of Champions","White River State Park\n " +
+                "801 W Washington St","awesome place2",0,0);
+        Result c = new Result("children_museum","The Children’s Museum of Indianapolis",
+                "3000 N Meridian St","awesome place3",0,0);
+        Result d = new Result("art_museum","Indianapolis Museum of Art",
+                "Indianapolis, IN 46208","awesome place4",0,0);
 
         ArrayList<Result> results1 = new ArrayList<Result>();
         ArrayList<Result> results2 = new ArrayList<Result>();
@@ -157,6 +160,58 @@ public class ResultActivity extends Activity {
                     Log.e("Address2", child.getReseult_description());
                     //Intent i = new Intent(ResultActivity.this, GoogleMapActivity.class);
                     //startActivity(i);
+
+                                    /*
+                * "http://maps.google.com/maps?saddr="
+                + Constants.latitude + ","
+                + Constants.longitude + "&daddr="
+                + latitude + "," + longitude
+
+        */
+
+                    double initLat = 0;
+                    double initLon = 0;
+                    double destLat = 0;
+                    double destLon = 0;
+
+                    if(child.getResult_name().equals("Lucas Oil Stadium"))
+                    {
+                        initLat = 39.766268;
+                        initLon = -86.168557;
+                        destLat =39.759862;
+                        destLon = -86.164017;
+                    }
+                    else if(child.getResult_name().equals("NCAA Hall of Champions"))
+                    {
+                        destLat = 39.768041;
+                        destLon = -86.171586;
+                        initLat =39.759862;
+                        initLon = -86.164017;
+
+                    }
+                    else if(child.getResult_name().equals("The Children’s Museum of Indianapolis"))
+                    {
+                        initLat = 39.766268;
+                        initLon = -86.168557;
+                        destLat =39.810473;
+                        destLon = -86.158085;
+
+                    }
+                    else if(child.getResult_name().equals("Indianapolis Museum of Art"))
+                    {
+                        destLat = 39.826051;
+                        destLon = -86.186052;
+                        initLat =39.810473;
+                        initLon = -86.158085;
+
+                    }
+
+                    String url = "http://maps.google.com/maps?saddr="+initLat+","+initLon
+                            + "&daddr=" + destLat+ "," + destLon;
+                    Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(url));
+                    intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+                    startActivity(intent);
+
                 }
             });
             return convertView;
